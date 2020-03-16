c9dd47f744544e7fdbf319fc6b21080834dd23e1
wip-abramson <wip.abramson@gmail.com>
2020-03-16 22:52:27 +0000

    remove blurb from banner

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -6,10 +6,10 @@ const BannerLanding = (props) => (
             <header className="major">
                 <h1>Landing</h1>
             </header>
-            <div className="content">
-                <p>Human focused tools for forming and extending trust in digital relationships,<br />
-                without infringing on citizens rights to privacy and freedom</p>
-            </div>
+            {/*<div className="content">*/}
+                {/*<p>Human focused tools for forming and extending trust in digital relationships,<br />*/}
+                {/*without infringing on citizens rights to privacy and freedom</p>*/}
+            {/*</div>*/}
         </div>
     </section>
 )
